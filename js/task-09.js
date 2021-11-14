3a48eb9bbdae2e0a4f80647cdba34f35cdf92391
moroz01 <aleksandr.moroz01@gmail.com>
2021-11-14 16:41:08 +0200

    Добавляет ДЗ 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtnEl = document.querySelector("button.change-color");
+const colorNameEl = document.querySelector("span.color");
+
+changeColorBtnEl.addEventListener("click", onColorChange);
+
+function onColorChange() {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  colorNameEl.textContent = `${getRandomHexColor()}`;
+}
